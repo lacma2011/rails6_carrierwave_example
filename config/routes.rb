7529a3e4af5d91cef86b35b1274f62312206fa7e
lacma2011 <lacma2011@github.com>
2022-05-23 21:51:13 -0700

    5.2.8

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'ads/index'
   get 'ads/edit/:id', to: 'ads#edit'
   post 'ads/edit/:id', to: 'ads#save'
-  root to: "ads#index"
+  root to: "ads#index"  
 end
